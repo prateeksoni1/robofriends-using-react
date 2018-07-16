ce40d5730e2680e1376a571e5b2b54d8ffe9c35a
proforty7 <proforty7@gmail.com>
2018-07-16 14:38:51 +0530

    finished

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
-import CardList from './CardList';
-import SearchBox from './SearchBox';
+import CardList from '../components/CardList';
+import SearchBox from '../components/SearchBox';
 import './App.css';
+import Scroll from '../components/Scroll';
 
 class App extends Component{
 
@@ -41,7 +42,9 @@ class App extends Component{
 			<div className = 'tc'>
 				<h1 className='f1'>Robofriends</h1>
 				<SearchBox searchChange={this.onSearchChanged}/>
-				<CardList robots = {filteredRobots} />
+				<Scroll>
+					<CardList robots = {filteredRobots} />
+				</Scroll>	
 			</div>
 		);
 	}
